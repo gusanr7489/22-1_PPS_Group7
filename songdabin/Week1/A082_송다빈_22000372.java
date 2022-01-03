22f5d3ea1300ac21cc7115d476b5546412adf370
Song Dabin <82192898+songdabin@users.noreply.github.com>
2022-01-03 21:15:09 +0900

    Create A082_송다빈_22000372.java

---
+++ songdabin/Week1/A082_송다빈_22000372.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class A082_송다빈_22000372 {
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		int n = s.nextInt();
+		int[] num = new int[10];
+		int[] res = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < 10; j++) num[j] = s.nextInt();
+			Arrays.sort(num);
+			res[i] = num[7];
+		}
+
+		for (int i = 0; i < n; i++) System.out.println(res[i]);
+	}
+	
+}
