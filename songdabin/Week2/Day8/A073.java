5367b0a6d2549299f0186025eaec18f65fe84456
Song Dabin <82192898+songdabin@users.noreply.github.com>
2022-01-12 18:57:13 +0900

    Create A073.java

---
+++ songdabin/Week2/Day8/A073.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class A073 {
+
+	public List<String> fizzBuzz(int n) {
+		ArrayList<String> res = new ArrayList<String>();
+		
+		for (int i = 1; i <= n; i++) {
+			if ((i % 3 == 0) && (i % 5 == 0))
+				res.add("FizzBuzz");
+			else if (i % 3 == 0)
+				res.add("Fizz");
+			else if (i % 5 == 0)
+				res.add("Buzz");
+			else
+				res.add(String.valueOf(i));
+		}
+		
+		return res;
+	}
+}
