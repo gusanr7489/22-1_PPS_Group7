7828bdb7b3605683d31b97b33f43f55bb7b6dd49
Song Dabin <82192898+songdabin@users.noreply.github.com>
2022-01-20 19:38:34 +0900

    Create A031.java

---
+++ songdabin/week3/day14/A031.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class A031 {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		int N = s.nextInt();
+		int M = s.nextInt();
+		
+		System.out.println((N * M) - 1);
+	}
+
+}
