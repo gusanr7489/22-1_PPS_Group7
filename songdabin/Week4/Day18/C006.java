32535d44852ed38cc1f936aa1d8106f0c4da4016
Song Dabin <82192898+songdabin@users.noreply.github.com>
2022-01-26 18:40:30 +0900

    Create C006.java

---
+++ songdabin/Week4/Day18/C006.java
@@ -0,0 +1,17 @@
+
+public class C006 {
+	public int firstUniqChar(String s) {
+		int[] alpha = new int[26];
+		
+		for (int i = 0; i < s.length(); i++) {
+			alpha[s.charAt(i) - 'a']++;
+		}
+		
+		for (int i = 0; i < s.length(); i++) {
+			if (alpha[s.charAt(i) - 'a'] == 1)
+				return i;
+		}
+		
+		return -1;
+	}
+}
