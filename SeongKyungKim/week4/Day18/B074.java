340a4d92155ef60465ad63e04214af402a565418
KimSeongKyung <89564518+KimSeongKyung@users.noreply.github.com>
2022-01-28 16:06:45 +0900

    Create B074.java

---
+++ SeongKyungKim/week4/Day18/B074.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int num) {
+        int count=0;
+    if(num==1) return 0;
+		while(count<500) {
+			if(num%2==0) num = num/2;
+			else if(num%2==1) num = num*3+1;
+			count++;
+			if(num==1) {
+                return count;
+            }      
+		}
+     
+        return -1;
+    }
+}
